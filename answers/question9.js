9b199648f4bd36fa93d5d95097b3ce68c8a31a68
Eshhaa11 <patellesha11@gmail.com>
2024-12-14 14:49:09 +0300

    answered question9

--- answers/question9.js
+++ answers/question9.js
@@ -1,5 +1,5 @@
 //Explain what NaN means in JavaScript.
 
 //NaN stands for "Not-a-Number."
-// It is a special value in JavaScript that represents an undefined or unrepresentable numerical result, particularly in floating-point calculations. 
+// It is a special value in JavaScript that represents an undefined or unrepresentable numerical result, particularly in the floating-point calculations. 
 //NaN is the result of operations that do not yield a valid number, such as dividing zero by zero or attempting to analyze a non-numeric string as a number.
