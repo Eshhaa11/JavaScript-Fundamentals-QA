d0441ab95376b226141ef32907e1aec66bb1d36f
Eshhaa11 <patellesha11@gmail.com>
2024-12-14 14:48:37 +0300

    answered q8

--- answers/question8.js
+++ answers/question8.js
@@ -1,5 +1,5 @@
 //What is the purpose of the typeof operator in JavaScript?
 
-//The typeof operator is used to determine the type of a variable or value.
+//The typeof operator is used to determine the type of a variable or a value.
 //It returns a string that indicates the type of the unevaluated operand. 
 //This is useful for debugging and understanding the data types you are working with in your code.
