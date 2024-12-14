75d7bf379f60d32f95bc32a6a76d75c77e55d556
Eshhaa11 <patellesha11@gmail.com>
2024-12-14 14:47:44 +0300

    answered question9

---
+++ answers/question10.js
@@ -0,0 +1,4 @@
+//How do you create a popup alert in JavaScript?
+
+//You can create a popup alert in JavaScript using the alert() function. 
+//This function displays a dialog box with a specified message that you want and an OK button.
