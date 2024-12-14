5cadeaf39ba7fdf726e0d046376b94601937c6e5
Eshhaa11 <patellesha11@gmail.com>
2024-12-14 14:07:09 +0300

    answered question1.

---
+++ answers/question1.js
@@ -0,0 +1,6 @@
+//What is JavaScript, and how does it differ from HTML and CSS?
+
+//Firstly, HTML is a structured language, which is used to structure the content on the web.
+//Secondly, CSS is a stylesheet language, which is used to style the elements of html on the web.
+//while
+//JavaScript, is a programming language which is used to add interactivity, aswell as the dynamic behavior of the web pages.
