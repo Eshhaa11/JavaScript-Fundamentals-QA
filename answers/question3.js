8671378d7cf04fdd9c8269d084eda465bf933472
Eshhaa11 <patellesha11@gmail.com>
2024-12-14 14:25:22 +0300

    answere question3

---
+++ answers/question3.js
@@ -0,0 +1,25 @@
+//What are variables in JavaScript, and how do you declare them?
+ 
+//Variables are like containers which are used to store data values
+//We have 3 ways in which we can declare them
+  //1) var
+  //2) let
+  //3) const    
+    
+//In Var and Let, you can re-assign the value but for Const you cannot.
+
+//Examples
+  //var:
+      //var name = "Esha";
+      //name = "Kripa";
+
+  //let:
+      //let age = 18;
+     //age = 19;
+
+  //const:
+   // const city = "New York";
+   // city = "Los Angeles";      
+
+     //(This will throw an error since in const the values cannot be re-assign)
+
