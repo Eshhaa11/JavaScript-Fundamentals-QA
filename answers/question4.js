d5891a06664ebca4f7b239fb772d0c0f5f0d1095
Eshhaa11 <patellesha11@gmail.com>
2024-12-14 14:29:48 +0300

    answered question4

---
+++ answers/question4.js
@@ -0,0 +1,22 @@
+//What are the differences between var, let, and const?
+
+//var: It is a  function/global scope, and can be re-assigned.
+//let: It is a block scope, and can be re-assigned.
+//const: It is a block scope, and cannot be re-assigned, and must be initialized.
+
+////In Var and Let, you can re-assign the value but for Const you cannot.
+
+//Examples
+  //var:
+      //var name = "Esha";
+      //name = "Kripa";
+
+  //let:
+      //let age = 18;
+     //age = 19;
+
+  //const:
+   // const city = "New York";
+   // city = "Los Angeles";      
+
+       //(This will throw an error since in const the values cannot be re-assign)
