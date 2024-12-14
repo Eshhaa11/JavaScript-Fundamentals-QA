f74386f7474db46f86219ade5bfeb0eb02804e9b
Eshhaa11 <patellesha11@gmail.com>
2024-12-14 14:32:15 +0300

    answered question5

---
+++ answers/question5.js
@@ -0,0 +1,6 @@
+//What is the output of the following code snippet?
+//let x = 5; 
+//x = 10; 
+//console.log(x);
+
+//The output will be 10, since in let you can re-assign a value and the re-assigned value was 10
