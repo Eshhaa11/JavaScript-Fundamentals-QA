1155d081c2cfe75c7f876fa8c70e5a28f8c1921b
Eshhaa11 <patellesha11@gmail.com>
2024-12-14 14:39:06 +0300

    answered question 6

---
+++ answers/question6.js
@@ -0,0 +1,8 @@
+//What are data types in JavaScript? List at least three examples.
+
+
+//Data types are categories that tell us what kind of data a variable can hold , aswell as they help us understand how to use that data in our programs.
+
+//1. Number: 1012, 424.7, 0, -1199
+//2. String: "Hello"
+//3. Boolean: true, false
