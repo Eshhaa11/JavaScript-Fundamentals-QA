eaff34f9b80734d426a29f43035aa4ef92b24aba
Eshhaa11 <patellesha11@gmail.com>
2024-12-17 16:07:50 +0300

    q15 done

---
+++ answers/question15.js
@@ -0,0 +1,8 @@
+//Write a JavaScript expression that evaluates whether a number is both greater than 10 and less than 20.
+
+let num = 12; 
+let isBetween = num > 10 && num < 20; 
+console.log(isBetween); 
+
+//In this expression, num > 10 checks if num is greater than 10, and num < 20 checks if num is less than 20. The && operator make sure that both conditions must be true for the isBetween to be true.
+
