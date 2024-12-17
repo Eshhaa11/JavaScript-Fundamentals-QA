d488cdff70529ac866c7efc9f75f8d832b5b9385
Eshhaa11 <patellesha11@gmail.com>
2024-12-17 15:49:36 +0300

    answered question 11

---
+++ answers/question11.js
@@ -0,0 +1,4 @@
+//What is the difference between == and === in JavaScript?
+
+// == Is the equal to operator(checks for the equality of values)
+// === Is the equal value and equal type operator and if the types are different it will return `false`
