c37b83f686b4a6bd0f5693c1cb2ea67eaac5bca4
Eshhaa11 <patellesha11@gmail.com>
2024-12-17 16:28:13 +0300

    q19 done

---
+++ answers/question19.js
@@ -0,0 +1,6 @@
+//Explain the concept of scope in JavaScript.
+//Scope in JavaScript refers to the visibility or accessibility of variables and functions in different parts of the code.
+// There are two main types of scope:
+
+//Global Scope: Variables declared outside of any function or block are in the global scope and can be accessed from anywhere in the code.
+//Local Scope: Variables declared within a function or block are in the local scope and can only be accessed within that function or block.
