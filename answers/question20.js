8723220cedb7801c7137dfa143725e5a5679bf64
Eshhaa11 <patellesha11@gmail.com>
2024-12-17 16:28:33 +0300

    q20 done

---
+++ answers/question20.js
@@ -0,0 +1,4 @@
+//What are arrow functions, and how are they different from regular functions?
+
+//Arrow functions are a more concise way to write function expressions in JavaScript. 
+//They use the => syntax and do not have their own this, which means they inherit this from the parent scope.
