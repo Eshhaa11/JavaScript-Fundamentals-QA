9281b1037ca7e411640872b63a3d7e18d231616a
Eshhaa11 <patellesha11@gmail.com>
2024-12-17 16:07:34 +0300

    q13 done

---
+++ answers/question13.js
@@ -0,0 +1,5 @@
+//List and explain three arithmetic operators in JavaScript.
+
+//1) % - Modulus (Remainder) - It is used to return the division remainder.
+//2) ** - Exponentiation - It is used to raise the first operand to the power of the second operand.
+//3) ++ - Increment - It is used to increment numbers.
