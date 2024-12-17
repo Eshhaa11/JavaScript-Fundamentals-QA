7ec8dbb8919efbded2bec1661976f3533a7e419d
Eshhaa11 <patellesha11@gmail.com>
2024-12-17 16:07:41 +0300

    q14 done

---
+++ answers/question14.js
@@ -0,0 +1,6 @@
+//What is the difference between logical operators && and ||?
+
+//&& (Logical AND): This operator returns true if both operands are true. If either operand is false, it returns false.
+
+//|| (Logical OR): This operator returns true if at least one of the operands is true. It returns false only if both operands are false. 
+
