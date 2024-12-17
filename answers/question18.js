f0eea51a0971517aceb58a458bf51acb0812af8a
Eshhaa11 <patellesha11@gmail.com>
2024-12-17 16:28:05 +0300

    q18 done

---
+++ answers/question18.js
@@ -0,0 +1,6 @@
+//What is the difference between function declarations and function expressions?
+
+//Function Declarations - These are defined using the function keyword followed by the function name. They can be called before they are defined in the code.
+
+//Function Expressions: These are defined by assigning a function to a variable. They are not hoisted, so you cannot call them before they are defined.
+
