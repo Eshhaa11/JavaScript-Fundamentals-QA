fdd8e3590d982022b3fef62dc64049fb570f7590
Eshhaa11 <patellesha11@gmail.com>
2024-12-18 16:08:26 +0300

    q36 done

---
+++ answers/question36.js
@@ -0,0 +1,6 @@
+//What is a loop in JavaScript, and why do we use it?
+
+//A loop in JavaScript is a control structure that allows you to execute a block of code repeatedly based on a specified condition.
+// Loops are used to automate repetitive tasks, iterate over collections (like arrays or objects), and perform actions multiple times without having to write the same code repeatedly.
+// They help in reducing code redundancy and improving efficiency.
+
