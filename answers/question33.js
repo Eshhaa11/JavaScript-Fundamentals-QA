60c520df426cf20cdcff96f7de3941a98fe83260
Eshhaa11 <patellesha11@gmail.com>
2024-12-18 15:50:50 +0300

    q33 done

--- answers/question33.js
+++ answers/question33.js
@@ -1,2 +1,14 @@
 // What is an object in JavaScript? Write an example.
 
+//An object in JavaScript is a collection of key-value pairs, where each key is a string (or Symbol) and the value can be of any data type, including other objects, arrays, functions.
+
+// Objects are used to store and organize data in a structured way.
+
+
+const person = {
+    name: "Esha",
+    age: 18,
+    isProfessor: false
+};
+console.log(person); 
+
