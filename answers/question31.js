b605cec48fe056d2abb796a7fdcc3f613e0ab013
Eshhaa11 <patellesha11@gmail.com>
2024-12-18 15:46:18 +0300

    q 31 done

---
+++ answers/question31.js
@@ -0,0 +1,7 @@
+//What is an array in JavaScript? Write an example.
+
+//An array in JavaScript is a special type of object that is used to store a collection of values.
+//Arrays can hold multiple values in a single variable and can contain elements of different types, including numbers, strings, and even other arrays or objects.
+
+const fruits = ["apple", "pineapple", "avocado"];
+console.log(fruits); 
