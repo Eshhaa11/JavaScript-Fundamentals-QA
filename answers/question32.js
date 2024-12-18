b8b867c45e511fc8a5540b72592fbc5391eec032
Eshhaa11 <patellesha11@gmail.com>
2024-12-18 15:46:29 +0300

    q32 done

---
+++ answers/question32.js
@@ -0,0 +1,4 @@
+//How can you access the third element in the following array?
+
+//You can access the third element of the array using its index. 
+// array indices start at 0, so the third element is at index 2.
