bff8009b95bce8d3858b8e1a779d2818df515d7f
Eshhaa11 <patellesha11@gmail.com>
2024-12-18 15:44:47 +0300

    q22 done

---
+++ answers/question22.js
@@ -0,0 +1,9 @@
+//How can you select an element in the DOM using JavaScript? (Give an example.)
+
+//You can select an element in the DOM using various methods provided by the document object. 
+//One common method is document.getElementById().
+
+
+const element = document.getElementById("myElement");
+console.log(element); 
+
