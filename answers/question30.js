c18df8ab5b9a2a120830885a17923f5a8bd4e41e
Eshhaa11 <patellesha11@gmail.com>
2024-12-18 15:46:06 +0300

    q30 done

---
+++ answers/question30.js
@@ -0,0 +1,33 @@
+//Write a script to toggle the visibility of a paragraph when a button is clicked.
+
+//<!DOCTYPE html>
+//<html lang="en">
+//<head>
+    //<meta charset="UTF-8">
+    //<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    //<title>Toggle Paragraph Visibility</title>
+    //<style>
+        //#myParagraph {
+            //display: block; 
+        //}
+   // </style>
+//</head>
+//<body>
+   // <button id="toggleButton">Toggle Paragraph</button>
+    //<p id="myParagraph">This is a paragraph that can be toggled.</p>
+
+    //<script>
+       // const button = document.getElementById("toggleButton");
+        // const paragraph = document.getElementById("myParagraph");
+
+        //button.addEventListener("click", function() {
+            
+            //if (paragraph.style.display === "none") {
+                //paragraph.style.display = "block"; // Show the paragraph
+            //} else {
+                //paragraph.style.display = "none"; // Hide the paragraph
+           // }
+       // });
+    //</script>
+//</body>
+//</html>
