e5192212cb2e18a0a8f6d3d3eaee888ac0fdadae
Eshhaa11 <patellesha11@gmail.com>
2024-12-18 16:19:58 +0300

    q40 done

---
+++ answers/question40.js
@@ -0,0 +1,9 @@
+// What is the purpose of the break and continue keywords in loops?
+
+//break: The break statement is used to exit a loop prematurely. 
+//When break is encountered, the loop stops executing, and control is transferred to the statement following the loop.
+
+//and 
+
+//continue: The continue statement is used to skip the current iteration of a loop and move to the next iteration. 
+//When continue is encountered, the remaining code in the loop for that iteration is skipped.
