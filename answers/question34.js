ee535e433709a4455d7d1011623c58f6b3cfd0c3
Eshhaa11 <patellesha11@gmail.com>
2024-12-18 15:56:29 +0300

    q34 done

---
+++ answers/question34.js
@@ -0,0 +1,7 @@
+//How do you add a new key-value pair to an existing object?
+
+//You can add a new key-value pair to an existing object in JavaScript using either dot notation or bracket notation. 
+//Dot notation is straightforward and is used when the key is a valid identifier.
+
+//Bracket notation is useful when the key is not a valid identifier or when the key is stored in a variable.
+
