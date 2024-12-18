64b9354f0ee0eb666f851f2149a5080653c5c6b7
Eshhaa11 <patellesha11@gmail.com>
2024-12-18 15:45:37 +0300

    q27 done

---
+++ answers/question27.js
@@ -0,0 +1,8 @@
+//What is the difference between innerHTML and textContent?
+ 
+//innerHTML: This property allows you to get or set the HTML content of an element.
+// It can include HTML tags, which will be parsed and rendered as HTML.
+
+//textContent: This property allows you to get or set the text content of an element.
+// It does not parse HTML tags; it treats them as plain text.
+
