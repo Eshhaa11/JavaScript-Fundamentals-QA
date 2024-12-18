8b411a0944612853e0fca1fa677f2a98b6dcc3c0
Eshhaa11 <patellesha11@gmail.com>
2024-12-18 15:45:56 +0300

    q29 done

---
+++ answers/question29.js
@@ -0,0 +1,5 @@
+//What is event bubbling in JavaScript?
+
+//Event bubbling is a type of event propagation in the DOM. 
+//When an event occurs on an element, it first runs the handlers on that element, and then it propagates (or "bubbles up") to its parent elements, triggering their event handlers in the process. 
+//This allows for event delegation, where a single event listener can manage events for multiple child elements.
